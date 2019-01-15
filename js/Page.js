1c285b78dbd268ba5db5079187d83e098b6085e8
Daniel Ibanez <dibanezalegria@yahoo.com>
2019-01-15 21:12:56 +0000

    Pagerank functional for ex2

--- js/Page.js
+++ js/Page.js
@@ -1,8 +1,9 @@
 "use strict";
 
 export default class Page {
-    constructor(id, keywords, links) {
-        this.keywords = keywords;
-        this.links = links
+    constructor(id) {
+        this.id = id;
+        this.keywords = [];
+        this.links = [];     // array of pages ids linking to this page
     }
 }
--- js/ex2.js
+++ js/ex2.js
@@ -4,19 +4,11 @@ import Matrix from 'ml-matrix';
 import Page from "./Page.js";
 
 (function () {
-    "use strict";
-
-    /**
-    * Number of nodes (pages) in the network.
-    */
-    const NPAGES = 4;
-
-
     /**
     * Create adjacency matrix as HTML table.
     *
     * @param {string} htmlElement html element to attach the table to
-    * @param {Array} values 2d array with values
+    * @param {Array} values Adjacency array (2D array)
     *
     */
     function drawAdjacencyMatrix(htmlElement, values) {
@@ -37,7 +29,8 @@ import Page from "./Page.js";
                     } else {
                         let value = values[row - 1][col - 1];
                         let active = (value == 1) ? "active" : "";
-                        td = `<td id="${row}.${col}">${value}</td>`;
+                        let disabled = (row == col) ? "disabled" : "";
+                        td = `<td id="${row}.${col}" class="${active} ${disabled}">${value}</td>`;
                     }
                 }
 
@@ -53,6 +46,11 @@ import Page from "./Page.js";
 
     /**
     * Calculate Hyperlink matrix.
+    *
+    * @param {Array} mat adjacency array (2D array)
+    *
+    * @return {Array} Hyperlink array2D
+    *
     */
     function calculateHyperlink(mat) {
         let hyp = [];
@@ -79,6 +77,11 @@ import Page from "./Page.js";
     /**
     * Covert matrix H to S to allow dangling nodes.
     * Formula: M = (1 − m)H + mS
+    *
+    * @param {Array} hyperMat hyperlink array2D
+    *
+    * @return {Matrix} M array (2d)
+    *
     */
     function modifyMatrix(hyperMat) {
         const m = 0.15;
@@ -97,11 +100,13 @@ import Page from "./Page.js";
 
     /**
     * Calculate PageRank.
+    *
+    * @param {Matrix} mat
+    *
+    * @return {Matrix} Matrix (4x1)
+    *
     */
-    function calculatePageRank(mat) {
-        // Modified M matrix
-        let modMat = new Matrix(mat);
-
+    function calculatePageRank(modMat) {
         // Vector
         let array2D = [];
         for (let i = 0; i < NPAGES; i++) {
@@ -114,7 +119,6 @@ import Page from "./Page.js";
         let k = 1;
         let iterate = true;
         do {
-            console.log("k: " + k);
             let mkMat = modMat;
             // Calculate M^k
             for (let i = 0; i < k - 1; i++) {
@@ -125,7 +129,7 @@ import Page from "./Page.js";
             rankMat = mkMat.mmul(vector);
 
             // Check whether condition to stop iteration is met.
-            if (isIterationOver(rankMat, prevMat)) {
+            if (isIterationOver(rankMat, prevMat, k)) {
                 iterate = false;
             } else {
                 prevMat = rankMat;
@@ -133,22 +137,31 @@ import Page from "./Page.js";
             }
         } while (iterate);
 
+        console.log("iterations: " + k);
+
         return rankMat;
     }
 
 
     /**
     * Check whether condition to stop iteration is met.
-    * Condition |r(k) − r(k − 1)| < ε with ε > 0
+    * Condition |r(k) − r(k − 1)| < ε with ε > 0.001
+    *
+    * @param {Array} actualMat PageRank for actual iteration
+    * @param {Array} prevMat PageRank for previous iteration
+    *
+    * @return {Boolean} true if condition to finish iteration is met.
+    *
     */
-    function isIterationOver(actualMat, prevMat) {
+    function isIterationOver(actualMat, prevMat, k) {
         if (prevMat == null) {
             return false;
         }
         let subsMat = Matrix.sub(actualMat, prevMat);
 
         for (let row = 0; row < NPAGES; row++) {
-            if (subsMat.get(row, 0) != 0) {
+            // ε > 0.001
+            if (Math.abs(subsMat.get(row, 0)) > 0.001) {
                 return false;
             }
         }
@@ -157,22 +170,132 @@ import Page from "./Page.js";
     }
 
 
+    /**
+    * Generate random network with given number of nodes and links.
+    *
+    * @param {integer} totalPages
+    * @param {integer} totalLinks
+    *
+    * @return {Array} array of Page objects
+    *
+    */
+    function generateRandomNetwork(totalPages, totalLinks) {
+        let pageArray = [];
+        for (let i = 0; i < totalPages; i++) {
+            let page = new Page(i + 1);
+            pageArray.push(page);
+        }
 
-    // Adjacency matrix (lecture) 
-    var mat = [
-        [0, 0, 1, 1],
-        [1, 0, 0, 0],
-        [1, 1, 0, 1],
-        [1, 1, 0, 0]
-    ];
+        // Assign links to pages
+        for (let i = 0; i < totalLinks; i++) {
+            // Index of page origin in array [0, totalPages - 1]
+            let pOrigin = Math.floor(Math.random() * totalPages);
+            // ID of page target
+            let targetId = Math.floor(Math.random() * totalPages) + 1;
+            let pageOrigin = pageArray[pOrigin];
+            if (targetId == pageOrigin.id || pageOrigin.links.includes(targetId)) {
+                i--;
+            } else {
+                pageArray[pOrigin].links.push(targetId);
+            }
+        }
 
+        // Assign keywords to pages
+        let words = ["dog", "cat", "horse", "chicken", "fish", "bear",
+            "bird", "shark", "snake", "pig", "lion", "turkey", "wolf", "spider"];
+
+        for (let i = 0; i < totalPages; i++) {
+            // One page can have [1, 3] keywords
+            let num = Math.floor(Math.random() * 3) + 1;
+            for (let key = 0; key < num; key++) {
+                let word = Math.floor(Math.random() * words.length);
+                pageArray[i].keywords.push(words[word]);
+            }
+        }
+
+        return pageArray;
+    }
+
+
+    /**
+    * Generate adjancency matrix from array of pages.
+    *
+    * @param {Array} pageArray array of Page objects
+    *
+    * @return {Array} Adjacency Array (2D array)
+    *
+    */
+    function createAdjacencyMatrix(pageArray) {
+        let mat = [];
+        for (let row = 0; row < NPAGES; row++) {
+            let aRow = Array(NPAGES).fill(0);
+                pageArray[row].links.forEach (function (link) {
+                    aRow[link - 1] = 1;
+                });
+            mat.push(aRow);
+        }
+        return mat;
+    }
+
+
+    /**
+    * Sort page rank matrix.
+    *
+    * @param {Matrix} mat
+    *
+    * @return {Array}   2d array with row [page number, PR value]
+    *                   sorted by PR value DESC
+    */
+    function sortPageRank(mat) {
+        // Extract Matrix to array 4x1
+        let array2d = [];
+        for (let row = 0; row < NPAGES; row++) {
+            array2d.push([row, mat.get(row, 0)]);
+        }
+
+        // Sort
+        array2d.sort(function compare(a, b) {
+            return b[1] - a[1];
+        });
+
+        return array2d;
+    }
+
+
+
+    /**
+    * Number of nodes (pages) and connections in the network.
+    * Note maximum number of NLINKS = NPAGES * (NPAGES - 1)
+    */
+    const NPAGES = 5;
+    const NLINKS = 20;
+
+    // Adjacency matrix (lecture)
+    // var mat = [
+    //     [0, 0, 1, 1],
+    //     [1, 0, 0, 0],
+    //     [1, 1, 0, 1],
+    //     [1, 1, 0, 0]
+    // ];
+
+    // Generate networks of pages
+    var pageArray = generateRandomNetwork(NPAGES, NLINKS);
+    var mat = createAdjacencyMatrix(pageArray);
 
     drawAdjacencyMatrix("table-div", mat);
     var hyperMat = calculateHyperlink(mat);
     var modMat = modifyMatrix(hyperMat);
+
+    // // DEBUG
+    // var modMat = new Matrix(hyperMat);
+    //
+    //
     var rankMat = calculatePageRank(modMat);
 
-
     console.log(rankMat);
+
+    var sortMat = sortPageRank(rankMat);
+
+    // console.log(rankMat);
 
 })();
